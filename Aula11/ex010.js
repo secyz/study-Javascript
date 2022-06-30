d3f244c67f5bfc606324193fa6c08bf3a005095d
Gabriel Lima <gabriellima4121@gmail.com>
2022-06-29 23:09:13 -0300

    commit

---
+++ Aula11/ex010.js
@@ -0,0 +1,8 @@
+var vel= 60.1
+console.log(`A velociade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // condição simplesa   
+    console.log('Voce Ultrapassou a velocidade permitida, MULTADO')
+}
+
+
+console.log('Diriga sempre usando cinto de segurança')
---
+++ Aula11/ex011.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'brasil') {
+  console.log(' Você é Estrangeiro')
+} else {
+    console.log('Você é Brasileiro')
+}
