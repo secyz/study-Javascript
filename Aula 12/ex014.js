55c499ed8a706516d7c20145724276a5f84794c5
Gabriel Lima <gabriellima4121@gmail.com>
2022-07-11 13:29:50 -0300

    commit

---
+++ Aula 12/ex014.js
@@ -0,0 +1,14 @@
+var agr = new Date()
+var hora = agr.getHours()
+
+console.log(`Agora são exatamente ${hora} horas.`)
+    if( hora < 12 ) {
+        console.log('Bom Dia')
+    } else {
+        if(hora <= 18) {
+            console.log('Boa Tarde')
+        } else {
+          if(hora > 18)
+          console.log('Boa Noite')
+        }
+    }
