7a0801db911ad99f936a245d6c58c595023c6642
Gabriel Lima <gabriellima4121@gmail.com>
2022-07-21 00:34:37 -0300

    commit

---
+++ Aula 17/RegEx/regex.js
@@ -0,0 +1,9 @@
+// g - global (encontra todas as ocorrências)
+// i - insensitive
+
+const { texto } = require('./text');
+
+const reexp1 = /joão/;
+
+
+console.log(reexp1.test(texto))
---
+++ Aula 17/RegEx/text.js
@@ -0,0 +1,15 @@
+const texto = `
+João trouxe flores para sua amada namorada em 10 de fevereiro de 1970,
+Maria era o nome dela.
+
+
+Foi um ano exelente na vida de joão. Teve 5 filhos, todos adultos atualmente.
+Maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de domingo. Também né! Sendo a boa mineira que é, nunca esquece seu famoso
+pão de queijo.
+Não canso de ouvir a Maria:
+"Joooooooãooooo, o café ta prontinho aqui. Veeemm"!
+`;
+
+module.exports = {
+    texto
+}
